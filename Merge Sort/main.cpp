cb6051a31a62638d33a29a123901b20ad7eea068
sairaj <sairaj2119@gmail.com>
2021-03-22 13:43:07 +0530

    add quick sort

---
+++ Merge Sort/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[]{5, 7, 8, 1, 2};
+
+    int la[]{1, 4, 5}, ra[]{2, 3, 6};
+    int res[6]{};
+    int c1{0}, c2{0};
+
+    return 0;
+}
